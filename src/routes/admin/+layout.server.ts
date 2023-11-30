f65a3eab0c1bec26ba45d944640e893a0a35e3b3
othavioquiliao <othavioaqa@gmail.com>
2023-11-30 18:00:42 -0300

    up

--- src/routes/admin/+layout.server.ts
+++ src/routes/admin/+layout.server.ts
@@ -1,21 +1,21 @@
-// import { redirect } from '@sveltejs/kit';
-// import type { LayoutServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
 
 //-------------- PARA TESTAR SO COMENTA ESTA VALIDACAO------------------
 // ----    QUE O SITE VAI FUNCIONAR SEM VALIDACAO DE USUARIO -----------
-// export const load: LayoutServerLoad = ({ locals, cookies }) => {
-// 	if (!locals.user) {
-// 		console.log('NAO TEM USUARIO SETADO NO LOCALS NEM COOKIE');
-// 		console.log('ESSE E O RESULTADO DO locals', locals.user);
+export const load: LayoutServerLoad = ({ locals, cookies }) => {
+	if (!locals.user) {
+		console.log('NAO TEM USUARIO SETADO NO LOCALS NEM COOKIE');
+		console.log('ESSE E O RESULTADO DO locals', locals.user);
 
-// 		throw redirect(307, '/');
-// 	} else if (locals.user.role === 'ADMIN') {
-// 		console.log('Voce é admin ');
-// 		throw redirect(307, '/admin');
-// 	} else if (locals.user.role === 'USER') {
-// 		console.log('Voce é user ');
-// 		throw redirect(307, '/user');
-// 	}
-// 	console.log('locals', locals.user.role);
-// 	console.log('cookie', cookies);
-// };
+		throw redirect(307, '/');
+	} else if (locals.user.role === 'ADMIN') {
+		console.log('Voce é admin ');
+		throw redirect(307, '/admin');
+	} else if (locals.user.role === 'USER') {
+		console.log('Voce é user ');
+		throw redirect(307, '/user');
+	}
+	console.log('locals', locals.user.role);
+	console.log('cookie', cookies);
+};
