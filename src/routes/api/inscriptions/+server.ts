d0da3d06abcaeb8142bc157f4ff8e88e84b8abd3
Guilherme Monteiro <guilherme.temonteiro@gmail.com>
2023-11-30 00:19:33 -0300

    Confirmação de inscrição por email

--- src/routes/api/inscriptions/+server.ts
+++ src/routes/api/inscriptions/+server.ts
@@ -1,15 +1,18 @@
 import prisma from '$lib/prisma';
 import { json } from '@sveltejs/kit';
+import * as nodemailer from 'nodemailer';
 
 // Endpoint para ver todas las inscripciones
 export async function GET() {
-	const inscriptions = await prisma.inscription.findMany({
-		include: {
-			User: true,
-			Event: true
-		}
-	});
-	return json(inscriptions);
+	// const inscriptions = await prisma.inscription.findMany({
+	// 	include: {
+	// 		User: true,
+	// 		Event: true
+	// 	}
+	// });
+	// return json(inscriptions);
+	sendEmail(9);
+	return json('batata');
 }
 
 // Endpoint para crear una nueva inscripción
@@ -62,5 +65,108 @@ export async function POST({ request }) {
 		}
 	});
 
+	await sendEmail(createInscription.id);
+
 	return json(createInscription, { status: 201 });
+}
+
+async function sendEmail(inscriptionId: number) {
+	// Email e senha de onde será enviado (outlook)
+	const email = '';
+	const password = '';
+
+	const inscription = await prisma.inscription.findUnique({
+		where: { id: Number(inscriptionId) },
+		include: { User: true, Event: true }
+	});
+
+	if (!inscription) {
+		return null;
+	}
+
+	const emailConfig = {
+		host: 'smtp.office365.com',
+		port: 587,
+		secure: false,
+		tls: {
+			rejectUnauthorized: false,
+			ciphers: 'SSLv3'
+		},
+		auth: {
+			user: email,
+			pass: password
+		}
+	};
+
+	const eventDate = inscription.Event.dateTime;
+
+	const formattedDate = new Date(eventDate).toLocaleString('pt-BR', {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+		second: 'numeric',
+		timeZoneName: 'short'
+	});
+
+	const html = `
+		<html lang="pt-BR">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<style>
+				body {
+					font-family: Arial, sans-serif;
+					background-color: #f4f4f4;
+					margin: 0;
+					padding: 0;
+				}
+		
+				.container {
+					max-width: 600px;
+					margin: 20px auto;
+					background-color: #fff;
+					padding: 20px;
+					border-radius: 8px;
+					box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+				}
+		
+				h1 {
+					color: #333;
+				}
+		
+				p {
+					color: #555;
+					line-height: 1.6;
+				}
+			</style>
+		</head>
+		<body>
+			<div class="container">
+				<h1>Olá, ${inscription.User.name}.</h1>
+				<p>Sua inscrição para ${inscription.Event.description} foi confirmada.</p>
+				<p>Aguardamos a sua presença!</p>
+				<p>O evento está agendado para ${formattedDate}</p>
+			</div>
+		</body>
+		</html>`;
+
+	const mailOptions = {
+		from: email,
+		to: inscription.User.email,
+		subject: `Confirmação de inscrição - ${inscription.Event.description}`,
+		html
+	};
+
+	const transporter = nodemailer.createTransport(emailConfig);
+
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			return json(error, { status: 400 });
+		} else {
+			return json(info.response);
+		}
+	});
 }
