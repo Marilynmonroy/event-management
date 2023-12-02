1d03d79f55b0d128c6f156e2ef6a7156dfdfd76d
Marilynmonroy <artlyn.monroy@gmail.com>
2023-12-02 20:41:39 -0300

    sou admin

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,6 +2,7 @@ export const load = async (event) => {
 	let session: string;
 	if (event.cookies.get('session_ADMIN')) {
 		session = 'ADMIN';
+		console.log('sou Admin');
 	} else if (event.cookies.get('session_USER')) {
 		session = 'USER';
 		console.log('sou user');
