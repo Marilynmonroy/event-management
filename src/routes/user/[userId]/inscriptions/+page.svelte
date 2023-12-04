8235d248544f82cabb8f81549ad7768015579a77
Marilynmonroy <artlyn.monroy@gmail.com>
2023-12-04 19:41:35 -0300

    inscriptions

--- src/routes/user/[userId]/inscriptions/+page.svelte
+++ src/routes/user/[userId]/inscriptions/+page.svelte
@@ -1,20 +1,44 @@
 <script lang="ts">
-	import type { PageData } from './$types';
 	import Cards from '$lib/components/Cards/Cards.svelte';
+	import { getToastStore, type ToastSettings } from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
+	const toastStore = getToastStore();
 
-	export let data: PageData;
+	let inscription: any[] = [];
 
-	let inscription = data.inscriptions;
+	onMount(async () => {
+		const res = await fetch(`/api/events/${3}/inscriptions`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+		if (res.ok) {
+			const data = await res.json();
+			inscription = data;
+
+			console.log('Dentro', inscription);
+		} else {
+			const t: ToastSettings = {
+				message: 'Erro ao carregar eventos',
+				timeout: 3000
+			};
+			toastStore.trigger(t);
+		}
+	});
 </script>
 
 <div class="m-20">
 	<h1 class="font-black text-4xl pb-10">Ingressos</h1>
-	<Cards
-		image={inscription.image}
-		title={inscription.title}
-		location={inscription.location}
-		dataEvent={inscription.dataEvent}
-		beginningEvent={inscription.beginningEvent}
-		endEvent={inscription.endEvent}
-	/>
+	{#each inscription as userInscription}
+		<Cards
+			image={userInscription.image}
+			title={userInscription.title}
+			location={userInscription.location}
+			dataEvent={userInscription.dataEvent}
+			beginningEvent={userInscription.beginningEvent}
+			endEvent={userInscription.endEvent}
+		/>
+	{/each}
 </div>
